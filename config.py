7fbc72f397f5a53f5400600cb8a0677b11940c11
Benny <benny.think@gmail.com>
2017-11-07 10:06:13 +0800

    remove

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ TOKEN = '353rUbWd-_xEb0'
 
 # Optional
 # if you leave TURING_KEY with blank, the robot won't chat with you.
-TURING_KEY = '***REMOVED***'
+TURING_KEY = ''
 
 # Optional, set it to 1 to output debug info in console
 DEBUG = '0'
