b4a66255e5b16a290c679e64dcce2d944cdc9254
johnpoint <jahanngauss414@gmail.com>
2017-11-12 16:06:50 +0800

    # update
    * 增加快捷操作
    * 增加查看状态
    * 修复菜单颜色不能正确显示问题
    * 修改readme

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ export PATH
 
 #=================================================
 #	System Required: Debian 6+/Ubuntu 14.04+
-#	Version: 2.0.0
+#	Version: 3.2.0
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #   Install Express Bot
@@ -13,7 +13,7 @@ export PATH
 #   警告！请勿用于生产环境！！！
 #=================================================
 
-sh_ver="2.0.0"
+sh_ver="3.2.0"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -32,12 +32,15 @@ if [ -f /etc/redhat-release ];then
  exit 1
  fi
 
+ #check_sys
+ check_sys(){
  if [[ ${OS} == 'CentOS' ]];then 
- echo '${Error} 抱歉，本脚本不支持此系统'
+ echo "${Error} 抱歉，本脚本不支持此系统"
  exit 1
  else 
- echo '${Info} 开始安装...'
+ echo -e "${Info} 开始执行脚本"
  fi
+ }
  
  #Install_all
  Install_all(){
@@ -86,19 +89,25 @@ Start_service
  #Start_service
  Start_service(){
 systemctl start expressbot.service
-echo '${Info}服务已启动'
+echo -e "${Info}服务已启动"
 }
 
 #Stop_service
 Stop_service(){
 systemctl stop expressbot.service
-echo '${Info}服务已停止'
+echo -e "${Info}服务已停止"
 }
 #Restart_service
 Restart_service(){
 Stop_service
 Start_service
 }
+#Service_status
+Service_status(){
+systemctl status expressbot.service
+}
+
+menu(){
 	echo -e "  Express Bot一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   ---- 主程序：BennyThink  | 脚本：johnpoint ----
   ——————————————————————
@@ -110,6 +119,7 @@ Start_service
   ${Green_font_prefix}4.${Font_color_suffix} 启动 服务
   ${Green_font_prefix}5.${Font_color_suffix} 停止 服务
   ${Green_font_prefix}6.${Font_color_suffix} 重启 服务
+  ${Green_font_prefix}7.${Font_color_suffix} 查看 服务状态
   ——————————————————————
  "
 	echo && stty erase '^H' && read -p "请输入数字 [1-15]：" num
@@ -132,7 +142,22 @@ case "$num" in
 	6)
 	Restart_service
 	;;
+	7)
+	Service_status
+	;;
 	*)
-	echo -e "${Error} 请输入正确的数字 [1-6]"
+	echo -e "${Error} 请输入正确的数字 [1-7]"
 	;;
 esac
+}
+check_sys
+action=$1
+if [[ ! -z $action ]]; then
+	if [[ $action = "start" ]]; then
+		Start_service
+	elif [[ $action = "stop" ]]; then
+		Stop_service
+	fi
+else
+	menu
+fi
