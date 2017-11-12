2bea628bceb477228be01304ad9093fde61390c8
Benny <benny.think@gmail.com>
2017-11-12 20:58:44 +0800

    tests

---
+++ tests/build.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding:utf-8
+
+# ExpressBot - build.py
+# 2017/11/12 20:52
+# 
+
+__author__ = 'Benny <benny@bennythink.com>'
+
+import compileall
+compileall.compile_dir(r'../expressbot')
