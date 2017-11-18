dc4697f91c57eff550ffadaef6b2faf44d4885a2
johnpoint <jahanngauss414@gmail.com>
2017-11-18 22:25:42 +0800

    # fix

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ export PATH
 #	System Requirements:
 #   Debian 6+, Ubuntu 14.04+, CentOS 7+,
 #   better with systemd.
-#	Version: 4.0.1
+#	Version: 4.1.0
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #   Maintain: BennyThink
@@ -17,7 +17,7 @@ export PATH
 #   USE AT YOUR OWN RISK!!!
 #=================================================
 
-sh_ver="4.0.1"
+sh_ver="4.1.0"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -139,6 +139,7 @@ Start_service(){
 check_systemd
 if [ $? -eq 0 ];then
     echo -e "${Info} 服务已启动"
+	exit 1
 else
     echo -e "${Info} 服务未启动"
 fi
@@ -155,6 +156,7 @@ Stop_service(){
 check_systemd
 if [ $? -eq 0 ];then
     echo -e "${Error} 服务未启动"
+	exit 1
 else
     echo -e "${Info} 正在停止服务"
 fi
