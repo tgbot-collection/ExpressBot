296d1c82c4386dd8b48327d34dce958e087d2964
johnpoint <jahanngauss414@gmail.com>
2017-11-19 09:03:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ export PATH
 #	System Requirements:
 #   Debian 6+, Ubuntu 14.04+, CentOS 7+,
 #   better with systemd.
-#	Version: 4.3.0
+#	Version: 4.3.1
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #   Maintain: BennyThink
@@ -17,7 +17,7 @@ export PATH
 #   USE AT YOUR OWN RISK!!!
 #=================================================
 
-sh_ver="4.3.0"
+sh_ver="4.3.1"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -137,7 +137,6 @@ systemctl daemon-reload
 # Start_service
 Start_service(){
 check_systemd
-systemctl status expressbot.service
 if [ $? -eq 0 ];then
   echo -e "${Info}服务已经启动"
   exit 0
@@ -145,7 +144,7 @@ else
   echo -e "${Info}服务正在启动"
   systemctl start expressbot.service
 fi
-systemctl status expressbot.service
+check_systemd
 if [ $? -eq 0 ];then
   echo -e "${Info}服务启动成功"
   exit 0
@@ -164,7 +163,7 @@ if [ $? -eq 0 ];then
 else
   echo -e "${Error}服务已经停止"
 fi
-systemctl status expressbot.service
+check_systemd
 if [ $? -eq 0 ];then
   echo -e "${Error}服务停止失败"
 else
