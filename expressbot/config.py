4c8de610d2a63bcb5e784779fbda06b1c37bee99
Benny <benny.think@gmail.com>
2017-12-06 12:51:00 +0800

    sensitive

--- expressbot/config.py
+++ expressbot/config.py
@@ -9,7 +9,7 @@ TOKEN = '353rUbWd-_xEb0'
 # Mandatory, db path,e.g.:
 # Linux:`/home/ExpressBot/expressbot/bot.db`
 # Windows:`C:\Users\Benny\Documents\ExpressBot\expressbot\bot.db`
-DB_PATH = r'C:\Users\***REMOVED***\Documents\ExpressBot\expressbot\bot.db'
+DB_PATH = r'C:\Users\Benny\Documents\ExpressBot\expressbot\bot.db'
 
 # Optional, if you leave TURING_KEY with blank, the robot won't chat with you.
 TURING_KEY = ''
