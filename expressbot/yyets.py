efba90e32859563222457b7d5d1551a089e5e6c6
Benny <benny.think@gmail.com>
2018-01-03 11:18:52 +0800

    YYeTs REST API

---
+++ expressbot/yyets.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# coding:utf-8
+
+# ExpressBot - yyets.py
+# 2018/1/3 11:01
+# YYeTs API
+
+__author__ = 'Benny <benny@bennythink.com>'
+
+
+import requests
+import json
+
+
+if __name__ == '__main__':
+    pass
