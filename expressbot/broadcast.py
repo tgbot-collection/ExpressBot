e4f3199ca5434d197d3fc208920eaa7c4999f19e
Benny <benny.think@gmail.com>
2018-01-31 16:09:34 +0800

    undone job count and script

--- expressbot/broadcast.py
+++ expressbot/broadcast.py
@@ -3,7 +3,7 @@
 
 # ExpressBot - broadcast.py
 # 2018/1/29 11:51
-# 
+# TODO: need fix
 
 __author__ = 'Benny <benny@bennythink.com>'
 
@@ -21,9 +21,19 @@ def broadcast(msg):
         send_message(chat_id[0], msg)
 
 
+def get_undone_count():
+    cmd = 'SELECT * FROM job WHERE done=?'
+    user_list = db.select(cmd, (0,))
+
+    print('The undone job count is %s' % len(user_list))
+
+
 if __name__ == '__main__':
+
     if len(sys.argv) != 2:
         print('参数错误')
+    elif sys.argv[1] == '0':
+        get_undone_count()
     else:
         # running under windows needs decode('gbk')
         broadcast(sys.argv[1])
---
+++ job.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export DB_PATH='/home/ExpressBot/expressbot/bot.db'
+python /home/ExpressBot/expressbot/broadcast.py 0
