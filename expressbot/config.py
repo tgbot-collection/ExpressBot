e3945c3ca6e35def2fecb9019ddb83c450a4e08b
Aura <aurelia.ma329@gmail.com>
2020-12-20 14:40:36 +0800

    environ

--- expressbot/config.py
+++ expressbot/config.py
@@ -5,9 +5,10 @@
 import os
 
 # Mandatory
-TOKEN = 'TOKEN'
+
+TOKEN = os.environ.get("TOKEN") or 'TOKEN'
 # Optional, if you leave TURING_KEY with blank, the robot won't chat with you.
-TURING_KEY = 'KEY'
+TURING_KEY = os.environ.get("TURING") or 'TURING'
 
 DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bot.db')
 # minutes
